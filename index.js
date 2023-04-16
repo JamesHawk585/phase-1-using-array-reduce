7aee3e8f23f2a6bcbd5a9ad3b2e0e376f05acee9
James Hawk <jph94880@gmail.com>
2023-04-16 17:06:45 -0500

    Adds my file.md

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+function batteryCounter(arr) {
+    return arr.reduce((acc, val) => acc + val, 0);
+}
+
+const totalBatteries = batteryCounter(batteryBatches);
+
+console.log(totalBatteries)
+
+
+/* function batteryCounter(arr, reducer, init) {
+    let accum = init;
+    for (const element of arr) {
+        accum = reducer(accum, element);
+    }
+    return accum;
+}
+
+const totalBatteries = batteryCounter(batteryBatches, (accum, val), 0);
+
+console.log(totalBatteries)*/
+
+
+
